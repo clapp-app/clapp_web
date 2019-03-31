f1351fa453f257af6a4239510adaf3064d29fd92
sven-zo <svenzo@outlook.com>
2019-03-31 15:24:41 +0200

    :sparkles: add contain component

---
+++ src/components/Contain/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './styles.scss'
+
+const { contain, content } = styles
+
+const Contain = ({ children }) => {
+  console.log(styles)
+  return (
+    <div className={contain}>
+      <div className={content}>{children}</div>
+    </div>
+  )
+}
+
+export default Contain
