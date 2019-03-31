b0ff6fdc03d43caba12c5c8d4efdaa6b6781e439
sven-zo <svenzo@outlook.com>
2019-03-31 16:06:33 +0200

    :sparkles: add animation to clappbutton

--- src/components/ClappButton/index.jsx
+++ src/components/ClappButton/index.jsx
@@ -1,13 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import posed from 'react-pose'
+import { easing } from 'popmotion'
 import styles from './styles'
 
-const { circle } = styles
+const { cubicBezier } = easing
+const materialEasing = cubicBezier(0.25, 0.8, 0.25, 1)
+
+const Circle = posed.div({
+  default: {
+    scale: 1,
+    boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)'
+  },
+  pressed: {
+    scale: 0.7,
+    boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+    transition: {
+      scale: {
+        duration: 100
+      },
+      boxShadow: {
+        duration: 100,
+        ease: materialEasing
+      }
+    }
+  }
+})
 
 const ClappButton = () => {
+  const [pressed, setPressed] = useState(false)
+
+  useEffect(() => {
+    console.log(pressed)
+  })
+
+  function mouseDown() {
+    setPressed(true)
+  }
+
+  function mouseUp() {
+    setPressed(false)
+  }
+
+  function mouseLeave() {
+    setPressed(false)
+  }
+
   return (
-    <div className={circle}>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/3/3d/Emojione_1F44F.svg" />
-    </div>
+    <Circle
+      onMouseDown={() => mouseDown()}
+      onMouseUp={() => mouseUp()}
+      onMouseLeave={() => mouseLeave()}
+      pose={pressed ? 'pressed' : 'default'}
+      className={styles.circle}
+    />
   )
 }
 
